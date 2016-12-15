89a39e2999a730644556a7d33f9c81fc4718e1e4
Murph Grainger <murph.grainger@gmail.com>
2016-12-15 15:59:27 -0700

    get route by id validated

--- routes/users.js
+++ routes/users.js
@@ -5,12 +5,28 @@ var queries = require('../db/queries');
 
 /* GET users listing. */
 router.get('/:id', function(req, res, next) {
-  queries.getUser(req.params.id)
-  .then(user => {
-    console.log(user);
-    res.json(user);
-  })
+    if (!isNaN(req.params.id)) {
+        queries.getUser(req.params.id)
+            .then(user => {
+                if (user) {
+                    delete user.password;
+                    res.json(user);
+                } else {
+                    resError(res, 404, 'User does not exist!');
+                }
+            });
+    } else {
+        resError(res, 500, 'Use a valid ID!');
+    }
+
 });
+
+function resError(res, statusCode, message) {
+    res.status(statusCode);
+    res.json({
+        message
+    });
+}
 
 
 
