dd266fd16813838e045536018c5bed16ec3af5cf
Murph Grainger <murph.grainger@gmail.com>
2016-12-16 10:57:50 -0700

    prepping for deployment

--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,14 @@
+require('dotenv').config()
+
 module.exports = {
 
     development: {
         client: 'pg',
         connection: 'postgres://localhost/crud_degree'
+    },
+    production: {
+        client: 'pg',
+        connection: process.env.DATABASE_URL + '?ssl=true'
     }
 
 };
